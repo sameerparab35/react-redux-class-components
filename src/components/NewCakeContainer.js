97ab2066e3f15f28cb54404d0a881f8573f75648
Sameer Parab <sameerparab35@gmail.com>
2021-09-19 20:13:33 +0530

    NewCakeContainer to  pass payload to action method

---
+++ src/components/NewCakeContainer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { buyCake } from '../redux';
+import { connect } from 'react-redux';
+import { useState } from 'react';
+
+function NewCakeContainer(props) {
+    const [number, setNumber] = useState(1);
+    return (
+        <div>
+            <h2>Number of Cakes - {props.numberOfCakes}</h2>
+            <input type="text" value={number} onChange={(e) => setNumber(e.target.value)}></input>
+            <button onClick={() => props.buyCake(number)}>Buy {number} Cake</button>
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+        numberOfCakes: state.cake.numberOfCakes
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        buyCake: (number) => dispatch(buyCake(number))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewCakeContainer);
