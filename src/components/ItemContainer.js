6459dba86c44726b3ca0dafafd7717fdbac426c9
Sameer Parab <sameerparab35@gmail.com>
2021-09-19 20:43:04 +0530

    dispatch the action based on props

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { buyCake, buyIceCream } from '../redux';
 
 function ItemContainer(props) {
     return (
         <div>
-            Item - {props.item}
+            <h2> Item - {props.item}</h2>
+            <button onClick={props.buyItem}>Buy Items</button>
         </div>
     )
 }
@@ -20,4 +22,15 @@ const mapStateToProps = (state, ownProps) => {
 
 }
 
-export default connect(mapStateToProps)(ItemContainer)
+const mapDispatchToProps = (dispatch, ownProps) => {
+    const dispatchFunction = ownProps.cake
+        ? () => dispatch(buyCake())
+        : () => dispatch(buyIceCream())
+
+    return {
+        buyItem: dispatchFunction
+    }
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemContainer)
